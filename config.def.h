5a4cf6786bf4b2e4d835dee132e5c5165705f03a
rallen <rcallen@gmail.com>
2022-12-31 16:49:15 -0800

    added volume_button

--- config.def.h
+++ config.def.h
@@ -43,9 +43,9 @@ static const char delimiter[] = { ' ', ' ', ' ', DELIMITERENDCHAR };
 
 static Block blocks[] = {
 /*      pathu                           pathc                           interval        signal */
-        { PATH("calendar.sh"),          NULL,                           30,             1},
-        { PATH("volume.sh"),            PATH("volume_button.sh"),       0,              2},
-        { PATH("cpu_temp.sh"),          PATH("cpu_temp_button.sh"),     1,              4},
-        { PATH("battery.sh"),           PATH("battery_button.sh"),      30,             3},
+        { PATH("sb-packages"),          NULL,                            0,             20},
+        { PATH("sb-volume"),            PATH("sb-volume_button"),          0,            10},
+        { PATH("sb-date"),          NULL,                            1,              4},
+        { PATH("sb-clock"),           NULL,                           30,             3},
         { NULL } /* just to mark the end of the array */
 };
