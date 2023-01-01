69abd18b538488a8ab2058a813691f142dc255b2
rallen <rcallen@gmail.com>
2022-12-31 19:48:35 -0800

    package button

--- config.def.h
+++ config.def.h
@@ -45,8 +45,8 @@ static Block blocks[] = {
 /*      pathu                           pathc                           interval        signal */
         { PATH("sb-cpu"),               PATH("sb-cpu_button"),           5,              7},
         { PATH("sb-memory"),            PATH("sb-memory_button"),        5,              6},
-        { PATH("sb-weather"),           PATH("sb-weather_button"),       600,            5},
-        { PATH("sb-packages"),          PATH("sb-packages_button"),      0,              1},
+        { PATH("sb-weather"),           PATH("sb-weather_button"),       3600,           5},
+        { PATH("sb-packages"),          PATH("sb-packages_button"),      3600,           1},
         { PATH("sb-volume"),            PATH("sb-volume_button"),        0,              2},
         { PATH("sb-date"),              NULL,                            60,             3},
         { PATH("sb-clock"),             NULL,                            30,             4},
